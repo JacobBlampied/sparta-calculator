ae0e962dff6bc63094f1c709a2af836a97c3ed20
Tech-A11 <tech-a11@Tech-A11s-MacBook-Air.local>
2018-09-12 09:08:32 +0100

    added functions

--- js/app.js
+++ js/app.js
@@ -9,22 +9,55 @@ console.log(number1);
 var number2 = Number(prompt('please choose the second number'));
 console.log(number2);
 
-if (op === 'a') {
-  alert ("The answer is " + (number1+number2));
-} else if (op === 's') {
-  alert ("The answer is " + (number1-number2));
-} else if (op === 'm')
-  alert ("The answer is " + (number1*number2));
-} else if (op === 'd') {
-  alert ("The answer is " + (number1/number2));
+var answera = add(number1, number2);
+
+var answers = (number1 - number2);
+
+var answerm = (number1 * number2);
+
+var answerd = (number1 / number2);
+
+// if (op === 'a') {
+//   alert ("The answer is " + (number1+number2));
+// } else if (op === 's') {
+//   alert ("The answer is " + (number1-number2));
+// } else if (op === 'm') {
+//   alert ("The answer is " + (number1*number2));
+// } else if (op === 'd') {
+//   alert ("The answer is " + (number1/number2));
+// }
+
+
+function add(number1, number2) {
+  var answera = (number1 + number2);
+  return answera;
+  console.log(answera);
 }
 
-// function add(number1, number2) {
-//   var answer = (number1 + number2);
-//   return answer;
-//   console.log(answer);
-// }
-//
-// if (op === 'a') {
-//    alert ("The answer is " + (answer));
-// }
+function sub(number1, number2) {
+  var answers = (number1 - number2);
+  return answers;
+  console.log(answers);
+}
+
+function mul(number1, number2) {
+  var answerm = (number1 * number2);
+  return answerm;
+  console.log(answerm);
+}
+
+function div(number1, number2) {
+  var answerd = (number1 / number2);
+  return answerd;
+  console.log(answerd);
+}
+
+if (op === 'a') {
+   alert (`The answer is ${answera}`);
+} else if (op === 's') {
+  alert (`The answer is ${answers}`);
+} else if (op === 'm') {
+  alert (`The answer is ${answerm}`);
+} else if (op === 'd') {
+  alert (`The answer is ${answerd}`);
+}
